b5b408baa43fe6e1690fe13563c339f391db8514
HUNcamper <selimnahimi@gmail.com>
2022-04-23 20:08:13 +0200

    torrent listing

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TorrentService } from "../../shared/services/torrent.service";
+import {Torrent} from "../../shared/models/Torrent";
 
 @Component({
   selector: 'app-main',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  torrents : Array<Torrent> = [];
 
-  constructor() { }
+  constructor(private torrentService: TorrentService) { }
 
   ngOnInit(): void {
+    this.torrentService.getAll().subscribe(data => {
+      this.torrents = data;
+    }, error => {
+      console.error(error);
+    });
   }
+
 
 }
