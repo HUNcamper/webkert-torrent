01218efaba2c9ed4088f059846f1eb7ccf179884
HUNcamper <selimnahimi@gmail.com>
2022-05-08 21:58:10 +0200

    environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'webkert-torrent',
+    appId: '1:569808903580:web:bd8cabe219cda156367c59',
+    storageBucket: 'webkert-torrent.appspot.com',
+    apiKey: 'AIzaSyDlYLkDkma7v0oQHCpHECr5EfQcfMA4QrE',
+    authDomain: 'webkert-torrent.firebaseapp.com',
+    messagingSenderId: '569808903580',
+  },
+  production: false,
+  hostUrl: 'http://localhost:4200'
+};
